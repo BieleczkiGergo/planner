09f5aa3afc275a9091c56879389f883a65dddefd
Geri <bgergo8b@gmail.com>
2022-03-04 09:18:54 +0100

    tried to work on functions again, SOMETHING doesn't work

--- functions.py
+++ functions.py
@@ -8,45 +8,36 @@ class timestamp:
         self.minute = minute
         self.timenum = self.hour*60 + self.minute
 
-    def update(self):
+    def updateNum(self):
         self.timenum = self.hour*60 + self.minute
 
+    def updateTime(self):
+        self.hour = self.timenum % 60
+        self.minute = self.timenum - (self.hour*60)
 
 
 class activity:
     events = []
 
 
-
 def rangeAND(range1, range2):
     state1 = False
     state2 = False
-    i1 = 0
-    i2 = 0
     stateU = False
     rangeU = activity()
-    while(bool(range1) and bool(range2)):
-        if range1.events[i1].timenum < range2.events[i2].timenum:
-            state1 = not(state1)
-            stateU = state1 and state2
-            state1.events.pop(0)
-            if stateU:
-                rangeU.events.append(range1.events[i1].timenum)
+    going = True
+    while going:
+        if bool(range1):
+            pass
 
-        elif range1.events[i1].timenum > range2.events[i2].timenum:
-            state2 = not(state2)
-            stateU = state1 and state2
-            state2.events.pop(0)
-            if stateU:
-                rangeU.events.append(range2.events[i2].timenum)
+    return rangeU
 
-        else:
-            state1 = not(state1)
-            state2 = not(state2)
-            stateU = state1 and state2
-            state1.events.pop(0)
-            state2.events.pop(0)
-            if stateU:
-                rangeU.events.append(range1.events[i1].timenum)
 
-        return rangeU
+act1 = activity
+act1.events = [timestamp(0, 20), timestamp(1, 10), timestamp(2, 40), timestamp(2, 52)]
+act2 = activity
+act2.events = [timestamp(0, 30), timestamp(1, 20), timestamp(2, 20), timestamp(2, 45)]
+
+available = rangeAND(act1, act2)
+for i in available.events:
+    print(f"Timestamp at hour: {i.hour}, minute: {i.minute}")
