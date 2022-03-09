e8c8222ad17c39be2bc8b44272c28ca3f01e2ff5
Geri <bgergo8b@gmail.com>
2022-03-09 09:47:29 +0100

    started working on CLI

--- CLI.py
+++ CLI.py
@@ -1,2 +1,10 @@
 from functions import *
 
+run = True
+print("Initialising")
+
+while run:
+    raw = input("planner>")
+    command = raw.split(" ")
+    if command[0].lower == "exit":
+        run = False
